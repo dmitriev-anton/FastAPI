8465109d0b3ab61e72270f860f26f102704ed16b
Anton Dmitriev <spaceboobman@gmail.com>
2024-04-18 14:10:50 +0400

    FastAPI Path class

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
 
-@app.get("/users/{id}")
-def users(id: int):
-    return {"user_id": id}
+@app.get("/users/{name}/{age}")
+def users(name: str = Path(min_length=3, max_length=20),
+          age: int = Path(ge=18, lt=111)):
+    return {"name": name, "age": age}
+
+@app.get("/users/{phone}")
+def users(phone:str  = Path(pattern=r"^\d{10}$")):
+    return {"phone": phone}
