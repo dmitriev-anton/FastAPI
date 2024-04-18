aa25e42dd594f6f67f0144df314aa22dd1cd43ba
Anton Dmitriev <spaceboobman@gmail.com>
2024-04-18 16:12:44 +0400

    FastAPI response status codes

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Response, Path
 
 app = FastAPI()
 
 
-@app.get("/users/{name}")
-def users(name: str = Path(min_length=3, max_length=20),
-          age: int = Query(ge=18, lt=111)):
-    return {"name": name, "age": age}
+@app.get("/users/{id}", status_code=200)
+def users(response: Response, id: int = Path()):
+    if id < 1:
+        response.status_code = 400
+        return {"message": "Incorrect Data"}
+    return {"message": f"Id = {id}"}
