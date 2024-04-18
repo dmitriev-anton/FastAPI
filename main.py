8cf75dba51a833087f65418bd30d5e04a7b6fc99
Anton Dmitriev <spaceboobman@gmail.com>
2024-04-18 15:26:29 +0400

    FastAPI Query class

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
 @app.get("/users")
-def get_model(name: str, age: int = 18):
-    return {"user_name": name, "user_age": age}
+def users(name: str | None = Query(default=None, min_length=2),
+          age: int = Query(ge=18, lt=111)):
+    if name == None:
+        return {"name": "Undefined", "age": age}
+    else:
+        return {"name": name, "age": age}
