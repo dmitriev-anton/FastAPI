85efcf7739d9e4fdf859f5f397b7d43bfcf99340
Anton Dmitriev <spaceboobman@gmail.com>
2024-04-18 15:29:44 +0400

    FastAPI Path + Query combining

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
 
-@app.get("/users")
-def users(name: str | None = Query(default=None, min_length=2),
+@app.get("/users/{name}")
+def users(name: str = Path(min_length=3, max_length=20),
           age: int = Query(ge=18, lt=111)):
-    if name == None:
-        return {"name": "Undefined", "age": age}
-    else:
-        return {"name": name, "age": age}
+    return {"name": name, "age": age}
